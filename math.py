a60ff9ea4abac612089ce5956c475cedbd8a48d6
abolfazlabedi1 <abediabolfazl1383@gmail.com>
2024-11-06 12:04:38 +0330

    Add files via upload

---
+++ math.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+delta = b ** 2 - 4 * a * c
+if delta > 0:
+    x1 = (-1 * b - delta ** 0.5) / 2 * a
+    x2 = (-1 * b + delta ** 0.5) / 2 * a
+print(x1)
+print(x2)
+elif delta = 0
+    x = (-1 * b) /(2 * a)
+print(x)
+    else :
+        print("no answer")
+
